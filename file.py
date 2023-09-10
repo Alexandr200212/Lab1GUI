956c1f6bf3ce9c9ca0fb914d9a2ff158a4181dea
Alexandr200212 <123861537+Alexandr200212@users.noreply.github.com>
2023-09-10 17:30:18 +0700

    shmykovPIE-01(2)

--- file.py
+++ file.py
@@ -7,3 +7,8 @@ def eratosthenes(n):
             for i in range(p*p, n+1, p):
                 is_prime[i] = False
     return primes
+
+def print_primes(n):
+    primes = eratosthenes(n)
+    for p in primes:
+        print(p, end=' ')
